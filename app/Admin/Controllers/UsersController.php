cec04f7a5eecba42dd7bf01305d503ce388d25ba
alex chan <331349470@qq.com>
2021-01-06 08:55:35 +0000

    create background for minjian

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class UsersController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = '用户管理';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User());
+
+        $grid->column('id', __('ID'));
+        $grid->column('name', __('姓名'));
+        $grid->column('avatar', __('头像'));
+        $grid->column('phone', __('电话'));
+        $grid->column('created_at', __('创建日期'));
+
+        $grid->disableCreateButton();
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->field('id', __('ID'));
+        $show->field('name', __('姓名'));
+        $show->field('avatar', __('头像'));
+        $show->field('phone', __('电话'));
+        $show->field('created_at', __('创建时间'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new User());
+
+        $form->text('name', __('姓名'));
+        $form->image('avatar', __('头像'));
+        $form->mobile('phone', __('电话'));
+        return $form;
+    }
+}
